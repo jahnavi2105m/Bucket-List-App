4f7242f74cb7458f5baa6933be20c6de2e5d65fc
jahnavi2105m <jahnavim2105@gmail.com>
2024-06-02 18:57:35 -0400

    Confirmation dialog update

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -56,7 +56,16 @@ export default {
   },
   methods: {
     deleteTask(index) {
-      this.tasks.splice(index, 1);
+      this.$q
+        .dialog({
+          title: 'Confirm',
+          message: 'Are you sure you want to delete?',
+          cancel: true,
+          persistent: true,
+        })
+        .onOk(() => {
+          this.tasks.splice(index, 1);
+        });
     },
   },
 };
