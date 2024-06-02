c17d7bb286e180e990286d7502b317610b92b390
jahnavi2105m <jahnavim2105@gmail.com>
2024-06-02 13:31:05 -0400

    checkbox colour update

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,9 +1,20 @@
 <template>
   <q-page class="bg-grey-3 column">
-    <q-list class="bg-white">
-      <q-item v-for="task in tasks" :key="task.title" v-ripple>
+    <q-list class="bg-white" separator bordered>
+      <q-item
+        v-for="task in tasks"
+        :key="task.title"
+        @click="task.done = !task.done"
+        :class="{ 'done bg-purple-1': task.done }"
+        clickable
+        v-ripple
+      >
         <q-item-section avatar>
-          <q-checkbox v-model="color" color="primary" />
+          <q-checkbox
+            v-model="task.done"
+            class="no-pointer-events"
+            color="primary"
+          />
         </q-item-section>
         <q-item-section>
           <q-item-label>{{ task.title }}</q-item-label>
@@ -35,3 +46,12 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+.done {
+  .q-item__label {
+    text-decoration: line-through;
+    color: #bbb;
+  }
+}
+</style>
