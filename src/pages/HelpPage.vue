10b5c23cca99a5717b96b3bf2b3d5dcc7e00a257
jahnavi2105m <jahnavim2105@gmail.com>
2024-06-02 12:57:35 -0400

    helppage update

--- src/pages/HelpPage.vue
+++ src/pages/HelpPage.vue
@@ -1,6 +1,14 @@
 <template>
-  <q-page>
-    <h5>Help</h5>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua.
+    </p>
   </q-page>
 </template>
 
